8648c1fafaae39dd9c5948cdaa066593d73bf37c
johan-hong <jhong@acceleratelearning.com>
2024-04-11 16:30:42 -0400

    use Aggregate Root pattern for payment entity

--- src/main/java/hotel/payment/Payment.java
+++ src/main/java/hotel/payment/Payment.java
@@ -1,25 +1,26 @@
 package hotel.payment;
 
+import hotel.payment.Payment.PaymentIdentifier;
+import hotel.reservation.Reservation;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import hotel.payment.Payment.PaymentIdentifier;
-import lombok.RequiredArgsConstructor;
 import org.jmolecules.ddd.types.AggregateRoot;
 import org.jmolecules.ddd.types.Identifier;
-import org.jmolecules.event.types.DomainEvent;
 import org.springframework.data.domain.AbstractAggregateRoot;
 
 import java.util.UUID;
 
-@Getter
 @Table(name = "payment")
-@RequiredArgsConstructor
+@Getter
 public class Payment extends AbstractAggregateRoot<Payment> implements AggregateRoot<Payment, PaymentIdentifier> {
 
     private final PaymentIdentifier id;
+    private final Reservation.ReservationIdentifier reservationId;
 
-    public record PaymentIdentifier(UUID id) implements Identifier {
+    public Payment(Reservation.ReservationIdentifier reservationId) {
+        this.id = new PaymentIdentifier(UUID.randomUUID());
+        this.reservationId = reservationId;
     }
 
-    public record PaymentCompleted(PaymentIdentifier id) implements DomainEvent {}
+    public record PaymentIdentifier(UUID id) implements Identifier {}
 }
